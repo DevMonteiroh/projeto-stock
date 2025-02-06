81b4740699549b30c6b5f338069f53ed6ac47390
DevMonteiroh <monteiro@amigosdedeus.com>
2025-02-06 15:41:39 -0300

    subindo meu projeto para o github

---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,106 @@
+import { MessageService } from 'primeng/api';
+import { UserService } from './../../services/user/user.service';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
+import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  loginCard = true;
+  loginForm = this.formBuilder.group({
+
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+
+  });
+
+  signupForm = this.formBuilder.group({
+
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+
+  });
+
+  constructor(private formBuilder: FormBuilder,
+    private userService: UserService,
+    private cookieService: CookieService,
+    private messageService: MessageService,
+    private router: Router
+  ) { }
+
+  onSubmitLoginForm(): void {
+
+    if (this.loginForm.value && this.loginForm.valid) {
+
+      this.userService.authUser(this.loginForm.value as AuthRequest)
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.cookieService.set('USER_INFO', response?.token);
+              this.loginForm.reset();
+              this.router.navigate(['/dashboard']);
+
+              this.messageService.add({
+                severity: 'success',
+                summary: 'sucesso',
+                detail: `Bem Vindo de volta ${response?.name}!`,
+                life: 2000,
+              });
+            }
+          },
+          error: (err) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: `Erro ao fazer login! ${err.message}`,
+              life: 2000,
+            });
+          }
+        });
+    }
+  }
+
+
+  onSubmitSignupForm(): void {
+    if (this.signupForm.value && this.signupForm.valid) {
+
+      this.userService.signupUser(this.signupForm.value as SignupUserRequest)
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              alert('Usuário criado')
+              this.signupForm.reset();
+              this.loginCard = true;
+
+              this.messageService.add({
+                severity: 'success',
+                summary: 'sucesso',
+                detail: `Usuário criado com sucesso!`,
+                life: 2000,
+              });
+            }
+          },
+          error: (err) => {
+
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: `Erro ao criar usuário!`,
+              life: 2000,
+            });
+          }
+        });
+    }
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+
+export const environment = {
+
+    production: true,
+    API_URL: 'http://localhost:3333',
+
+};
